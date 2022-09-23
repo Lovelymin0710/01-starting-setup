404b8bd8f9e6aed58e23eb3b005f34afc76c0c38
KYOUNGMIN <48938663+Lovelymin0710@users.noreply.github.com>
2022-09-23 17:25:30 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ExpenseItem from "./components/ExpenseItem";
+import react from "react";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem/>
+      <ExpenseItem/>
+      <ExpenseItem/>
+      
+    </div>
+  );
+}
+
+export default App;
